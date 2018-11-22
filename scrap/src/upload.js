13d3ada86531d532c9c5a480f32ab341761c896b
Jagadeesh <jagadeeshthegeek@gmail.com>
2018-11-21 18:44:19 -0800

    upload large s3 --added

--- scrap/src/upload.js
+++ scrap/src/upload.js
@@ -66,7 +66,16 @@ async function getMp4VideoUrl (allMovies) {
         const url = 'https://einthusan.tv' + eachMovie.enLink;
 
         console.log((count) + '--------------'+url);
-        const eachMovieVideoUrl = await getPageVideoUrl(url);
+        // const eachMovieVideoUrl = await getPageVideoUrl(url);
+
+        const eachMovieVideoUrl = {
+          "MP4Link": "https://s3-us-west-2.amazonaws.com/indiantv/cjorbezcy00001qqmc7931keo.mp4",
+          "HLSLink": "https://s19.einthusan.tv/einthusancom/hot/DzAq4.mp4.m3u8?e=1542876154&md5=y5ErhYvnUC7ZL1xWQz42nw",
+          "Datacenter": "San",
+          "Premium": false,
+          "V": 1
+        };
+
         eachMovie.videoUrl = eachMovieVideoUrl;
         console.log(eachMovieVideoUrl);
 
